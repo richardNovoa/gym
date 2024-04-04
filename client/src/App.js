62d7ae803a6f148ba58d1279677ef26ade1b9f90
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2024-04-04 17:54:47 -0400

    install tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='text-2xl'>Hello World</h1>
+      <p className='text-indigo-800'>This means that tailwind works</p>
+      <p className='text-red-800'>This means that tailwind works</p>
     </div>
   );
 }
