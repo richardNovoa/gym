277173236a6fdaced01c43f49bcb1f31c3f8acb1
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2024-04-05 23:18:59 -0400

    use Props in components

--- client/src/lib/components/ExerciseCard.js
+++ client/src/lib/components/ExerciseCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const ExerciseCard = () => {
-  return <div>This is an exercise card</div>;
+const ExerciseCard = ({ exercise }) => {
+  const slug = exercise.name.replace(/ /g, '_');
+  return (
+    <div className='flex text-left'>
+      <img
+        src={`https://raw.githubusercontent.com/wrkout/exercises.json/master/exercises/${slug}/images/0.jpg`}
+        className='w-48 object-cover object-top h-32'
+      />
+      <div className='pl-4'>
+        <h2 className='text-xl font-regular'>{exercise.name}</h2>
+        <p>{exercise.equipment}</p>
+        <p>{exercise.primaryMuscles}</p>
+        <p>{exercise.category}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ExerciseCard;
--- client/src/lib/components/ExerciseList.js
+++ client/src/lib/components/ExerciseList.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getExercises } from '../../events/exercise';
 import ExerciseCard from './ExerciseCard';
 
 const ExerciseList = () => {
+  const dispatch = useDispatch();
+  const exercises = useSelector((state) => state.exercises.exercises); // Access exercises from state
+
+  React.useEffect(() => {
+    dispatch(getExercises()); // Fetch exercises on component mount
+  }, [dispatch]);
+
   return (
     <div>
-      <ExerciseCard />
-      <ExerciseCard />
-      <ExerciseCard />
+      {exercises.length > 0 ? (
+        exercises.map((exercise) => (
+          <ExerciseCard exercise={exercise} key={exercise._id} />
+        ))
+      ) : (
+        <p>Loading exercises...</p> // Display loading message while fetching
+      )}
     </div>
   );
 };
