29f6f501e5bcfe577290d822944d30bee3f67186
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2024-04-05 23:18:20 -0400

    configure cors

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 
-const authPath = require('./routes/api/auth');
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // Connect database
 connectDB();
 
